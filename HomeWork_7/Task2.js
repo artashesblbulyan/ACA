5af5359ad04f58d2d735bf79c0ebff6726d6193c
artashesblbulyan <artshesblbulyan@gmail.com>
2022-11-22 21:16:27 +0400

    homework 7

--- HomeWork_7/Task2.js
+++ HomeWork_7/Task2.js
@@ -1,1 +1,26 @@
-sdfgsd
+// Exercise 2
+// Create a function that finds the highest integer in the array using recursion.
+// Note:
+// Suppose the array has at least one element.
+// Input Output
+// [1,2,3,4] 10
+// [1,4,6,2,44,56,9] 44
+// [2] 2
+// [1,1,1] 1
+
+function highestInteger(arr) {
+    let max = arr[0]
+    if(arr.length === 1){
+        return max
+    }
+    arrPop = arr.pop() 
+    if(arr[0] < arrPop){
+        arr[0] = arrPop
+    }
+    
+    
+  return highestInteger(arr)
+
+}
+
+console.log(highestInteger([9]))
